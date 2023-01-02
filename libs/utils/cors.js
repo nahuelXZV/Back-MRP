22cdf4f86e86f75438447e3df3b19b549f50a9c8
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-01-02 10:09:40 -0400

    cors libre

--- libs/utils/cors.js
+++ libs/utils/cors.js
@@ -5,11 +5,11 @@ const whiteList = [config.FRONTEND_URL];  // Whitelist
 const corsOptions = {
   origin: (origin, callback) => {
     const existe = whiteList.some(dominio => dominio === origin);
-    if (existe || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('No permitido'));
-    }
+    // if (existe || !origin) {
+    callback(null, true);
+    // } else {
+    // callback(new Error('No permitido'));
+    // }
   }
 }
 
